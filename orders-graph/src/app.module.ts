a990b3a9c1974f829d39e90ad9233b0b6c2877c9
M SREERAM REDDY <125948864+sreeram2124@users.noreply.github.com>
2025-02-13 17:18:13 +0530

    Creating kafka connect connectors

---
+++ orders-graph/src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { OrdersModule } from './orders/orders.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmConfig } from './config';
+import { HealthController } from './health/health.controller';
+import { TerminusModule } from '@nestjs/terminus';
+
+@Module({
+  imports: [
+    TerminusModule,
+    TypeOrmModule.forRoot(TypeOrmConfig),
+    GraphQLModule.forRoot({
+      autoSchemaFile: true,
+    }),
+    OrdersModule
+  ],
+  controllers: [HealthController],
+  providers: [],
+})
+export class AppModule {}
